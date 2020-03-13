d1096fb222a1e319fdb46adeb740c6a61f055584
Daniel Abler <djs.abler@gmail.com>
2020-03-13 13:08:44 +0100

    added overwrite flag to study extension

--- test_study/extend_parametric_study.py
+++ test_study/extend_parametric_study.py
@@ -8,15 +8,18 @@ ps.reload_state()
 #ps.collect_results(verbose=True)
 #ps.create_summary_doc()
 
-experiment = {"D_WM":      {'range' : [0.05,0.20], "stepsize" : 0.01},
-              "rho_WM":    {'range' : [0.02,0.20], "stepsize" : 0.01},
-              "coupling":  {'range' : [0.02,0.20], "stepsize" : 0.01}}
+# experiment = {"D_WM":      {'range' : [0.05,0.20], "stepsize" : 0.01},
+#               "rho_WM":    {'range' : [0.02,0.20], "stepsize" : 0.01},
+#               "coupling":  {'range' : [0.02,0.20], "stepsize" : 0.01}}
 
 
 # new combinations
-new_D_WM     = [0.18, 0.2, 0.22, 0.24, 0.26, 0.30]
-new_rho_WM   = [0.18, 0.2, 0.22, 0.24, 0.26, 0.30]
-new_coupling = [0.18, 0.2, 0.22, 0.24, 0.26, 0.30]
+# new_D_WM     = [0.18, 0.2, 0.22, 0.24, 0.26, 0.30]
+# new_rho_WM   = [0.18, 0.2, 0.22, 0.24, 0.26, 0.30]
+# new_coupling = [0.18, 0.2, 0.22, 0.24, 0.26, 0.30]
+new_D_WM     = [0.01, 0.02, 0.03]
+new_rho_WM   = [0.12]
+new_coupling = [0.02, 0.12]
 param_values = [new_D_WM, new_rho_WM, new_coupling]
 param_names  = ["D_WM", "rho_WM", "coupling"]
 
